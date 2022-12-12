864cd758fe7f196522d4d342c8a8c3897a4f53f8
0502j <jamiledsousa@gmail.com>
2022-12-12 15:47:45 -0300

    Update js-basics - Working with variables and constants

--- Javascript/js-basics/index.js
+++ Javascript/js-basics/index.js
@@ -1,1 +1,14 @@
+/*Starting point
 console.log('Hello World');
+*/
+
+//working with variables and constants
+let message = 'Hi';        //string literal
+let number  = "0";        //number literal
+let validate = true;     //boolean literal
+let ex;                 //undefined
+let example = null; 
+const interestRate = 0.3; //constant
+
+console.log(message);
+console.log(interestRate);
