815cdbfaebee90a7ece841ae997ffbcf3a666c11
0502j <jamiledsousa@gmail.com>
2022-12-12 17:47:15 -0300

    Update js-basics - testing operators

--- Javascript/js-basics/index.js
+++ Javascript/js-basics/index.js
@@ -28,7 +28,7 @@ person[selection] = "Empty";
 console.log(person.name);
 console.log(person.age);
 
-*/
+
 
 //arrays
 let selectedColors = ['red','blue'];
@@ -41,3 +41,56 @@ function greet(name){
 }
 
 greet("Jamile");
+*/
+
+//operators
+let x=10;
+let y=3;
+
+//arithmetic operator
+console.log(x++);
+console.log(x);
+
+//assignment operator
+x+=5;
+console.log(x);
+
+//comparison operator
+console.log(y>3);
+console.log(x===16);
+
+console.log("==================");
+//strict equality (same type and same value)
+console.log(1===1); //true
+console.log('1'===1); //false
+
+//lose equality (value)
+console.log(1==1); //true
+console.log('1'==1); //true
+console.log(true==1); //true
+
+console.log("==================");
+
+//ternary operator
+let points=50;
+let type= points > 100 ? 'gold' : 'silver';
+
+console.log(type);
+
+
+console.log("==================");
+//logical & non-booleans - returns true if both operands are true
+console.log(true && true); //true
+console.log(true && false); //false
+
+console.log("==================");
+
+let highIncome = true;
+let goodCreditScore = false;
+let eligible = highIncome && goodCreditScore;
+console.log(eligible); //true
+
+
+
+
+
