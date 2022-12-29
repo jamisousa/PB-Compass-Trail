7e10adbfd8f34e0fdb671841167101a978dac052
0502j <jamiledsousa@gmail.com>
2022-12-29 11:25:14 -0300

    Update react project - adding expense form

--- React/starting-project/src/App.js
+++ React/starting-project/src/App.js
@@ -1,23 +1,27 @@
-import Expenses from "./components/Expenses/Expenses";
+import React from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
+const App = () => {
   const expenses = [
     {id: 'e1', title: 'Car insurance', amount: 294.67, date: new Date(2021,2,15)},
-    {id: 'e2', title: 'Car insurance', amount: 155.25, date: new Date(2021,3,25)},
-    {id: 'e3', title: 'Car insurance', amount: 205.28, date: new Date(2022,1,30)},
-    {id: 'e4', title: 'Car insurance', amount: 144.99, date: new Date(2021,5,2)}
-  ]
+    {id: 'e2', title: 'New tv', amount: 155.25, date: new Date(2021,3,25)},
+    {id: 'e3', title: 'Wooden desk', amount: 205.28, date: new Date(2022,1,30)},
+    {id: 'e4', title: 'New bike', amount: 144.99, date: new Date(2021,5,2)}
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible</p>
-     
-     <Expenses items={expenses}></Expenses>
-
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
-    
   );
 }
 
