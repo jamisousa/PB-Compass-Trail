9969e6c8166ac981bfd5d4cc1f625c92689f1050
0502j <jamiledsousa@gmail.com>
2022-12-29 11:44:07 -0300

    Update react project - adding filter selection

--- React/starting-project/src/components/Expenses/Expenses.js
+++ React/starting-project/src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import React, {useState} from 'react';
 
 function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
+    <div>
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,6 +37,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
     </Card>
+    </div>
   );
 }
 
