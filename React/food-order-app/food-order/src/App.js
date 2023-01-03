235223a022c454125343829355aefa6735d46e5a
0502j <jamiledsousa@gmail.com>
2023-01-03 14:32:54 -0300

    Update react food-order-app - adding meals components

--- React/food-order-app/food-order/src/App.js
+++ React/food-order-app/food-order/src/App.js
@@ -1,10 +1,17 @@
 import React, {Fragment} from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
+
       <Header></Header>
+
+      <main>
+        <Meals></Meals>
+      </main>
+
     </React.Fragment>
   );
 }
