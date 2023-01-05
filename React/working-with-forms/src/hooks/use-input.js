4bea293ca2304a4577ed94a24c70fb5885e47f3f
0502j <jamiledsousa@gmail.com>
2023-01-05 18:07:16 -0300

    Update react working-with-forms project - using useReducer()

--- React/working-with-forms/src/hooks/use-input.js
+++ React/working-with-forms/src/hooks/use-input.js
@@ -1,27 +1,50 @@
-import {useState} from 'react';
+import {useReducer} from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+
+}
+
+const inputStateReducer = (state, action) => {
+  if(action.type === 'CHANGE'){
+    return {value: action.value, isTouched: state.isTouched}
+  }
+
+  if(action.type==='BLUR'){
+    return {value: state.value, isTouched: true}
+  }
+
+  if(action.type === 'RESET'){
+    return initialInputState;
+  }
+
+  return initialInputState;
+};
+
 
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
 
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = event => {
-        setEnteredValue(event.target.value);
+        dispatch({type: 'CHANGE', value:event.target.value});
       }
 
     const InputBlurHandler = event => {
-        setIsTouched(true);
+      dispatch({type: 'BLUR'});
       }
 
     const reset = () => {
-        setEnteredValue('');
-        setIsTouched(false);
+      dispatch({type: 'RESET'});
+
     }
   
     return {
-        value: enteredValue,
+        value: inputState.value,
         isValid: valueIsValid,
         hasError,
         valueChangeHandler,
