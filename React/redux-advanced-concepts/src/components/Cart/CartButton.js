aac55a4857bf05383f5bb0f15d627306079347b4
0502j <jamiledsousa@gmail.com>
2023-01-10 15:03:41 -0300

    Adding redux-advanced-concepts project

---
+++ React/redux-advanced-concepts/src/components/Cart/CartButton.js
@@ -0,0 +1,24 @@
+import classes from './CartButton.module.css';
+import {uiActions} from '../../store/ui-slice';
+import {useDispatch, useSelector} from 'react-redux';
+
+
+const CartButton = (props) => {
+
+  const cartQuantity = useSelector(state=>state.cart.totalQuantity);
+
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button onClick={toggleCartHandler} className={classes.button}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
---
+++ React/redux-advanced-concepts/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
+import './index.css';
+import store from './store/index';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Provider store={store}><App /></Provider>);
