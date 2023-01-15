92bd33181380f70cb48feb4d0310a708f81ae111
0502j <jamiledsousa@gmail.com>
2023-01-15 10:34:36 -0300

    Adding react studying-animations project

---
+++ React/studying-animations/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+import CSSTransition from 'react-transition-group/CSSTransition';
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false
+  }
+
+  showModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <button onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br></br>
+        <Transition 
+          in={this.state.showBlock} 
+          timeout={300} 
+          mountOnEnter 
+          unmountOnExit
+          >
+          {state=>(
+            <div style={{backgroundColor: "red", width:100, height:100, margin:"auto",
+            transition: 'opacity 1s ease-out',
+            opacity: state==="exiting" ? 0 : 1}}></div>
+
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
