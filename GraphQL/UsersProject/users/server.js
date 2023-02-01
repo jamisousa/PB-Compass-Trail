4e10e9376843f7f5399c7a169f58ae894e0f15aa
0502j <jamiledsousa@gmail.com>
2023-02-01 16:39:45 -0300

    Adding GraphQL Users project

---
+++ GraphQL/UsersProject/users/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+ 
+const app = express();
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    })
+);
+ 
+app.listen(4000, () => {
+    console.log('Server started at:');
+    console.log('http://localhost:4000');
+});
