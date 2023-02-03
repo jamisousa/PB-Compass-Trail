a4772cd648d1d0b4ed9e7220819dde500dc9c391
0502j <jamiledsousa@gmail.com>
2023-02-03 09:57:04 -0300

    Update GraphQL Lyrical project
    Working with queries, frontend mutations & automatic data caching

--- GraphQL/Lyrical/client/components/SongList.js
+++ GraphQL/Lyrical/client/components/SongList.js
@@ -1,23 +1,51 @@
-import React, {Component} from 'react';
-import gql from 'graphql-tag';
-import {graphql} from 'react-apollo';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/fetchSongs";
 
 class SongList extends Component {
-    render(){
-        return(
-            <div>
-            <p>SongList</p>
-            </div>
-        );
+  onSongDelete(id) {
+    this.props
+      .mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
+  renderSongs() {
+    return this.props.data.songs.map(({ id, title }) => {
+      return (
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
     }
+
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
+  }
 }
 
-const query = gql`
-    {
-        songs{
-        title
-        }
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
     }
+  }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(graphql(query)(SongList));
--- GraphQL/Lyrical/client/index.js
+++ GraphQL/Lyrical/client/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
-import {ApolloProvider} from 'react-apollo';
-import SongList from './components/SongList';
+import "./style/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import SongList from "./components/SongList";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import App from "./components/App";
+import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <div>Lyrical</div>
-        <SongList></SongList>
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+        <Route path="songs/new" component={SongCreate}></Route>
+        <Route path="songs/:id" component={SongDetail}></Route>
+      </Router>
     </ApolloProvider>
-  )
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
