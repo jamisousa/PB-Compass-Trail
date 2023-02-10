5210bd4cb6d288b1a224115318ca3d99ecfc3dfb
0502j <jamiledsousa@gmail.com>
2023-02-10 16:13:18 -0300

    Adding TS-Tests folder

---
+++ TS-Tests/FirstProject/src/app/Utils.ts
@@ -0,0 +1,15 @@
+import { UrlWithParsedQuery, parse } from "url";
+
+export class Utils {
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    if (!url) {
+      throw new Error("Empty url!");
+    }
+    return parse(url, true);
+  }
+
+  /* istanbul ignore next */
+  public static toUpperCase(arg: string): string {
+    return arg.toUpperCase();
+  }
+}
---
+++ TS-Tests/TSTestCourse/jest.config.itest.js
@@ -0,0 +1,10 @@
+module.exports = {
+    roots: ['<rootDir>/src/itest'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    verbose: true,
+    testEnvironment: "node"
+}
